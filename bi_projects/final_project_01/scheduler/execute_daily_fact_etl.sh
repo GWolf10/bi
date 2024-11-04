4daff22a7a07bcbc275a6a6c36eecc37aafe8558
GAl Wolf <galwolf10@gmail.com>
2024-11-04 17:04:01 +0200

    my first commit

--- bi_projects/final_project_01/scheduler/execute_daily_fact_etl.sh
+++ bi_projects/final_project_01/scheduler/execute_daily_fact_etl.sh
@@ -3,6 +3,6 @@
 #0 6 * * *  bash ~/workspace/bi/bi_projects/final_project_01/scheduler/execute_daily_fact_etl.sh
 # Run the python script according to our configuration
 
-cd workspace/bi
+cd ~/workspace/bi
 
 python ./bi_projects/final_project_01/final_project_01.py strange-bay-433318-g0 --etl-action daily --etl-name fact_etl
